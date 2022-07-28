2caa856eae9a36ec6e622b51f2c12e3a90481e55
ryancho-hakhyeon <ryancho.hakheyon@gmail.com>
2022-07-28 03:20:09 -0700

    next day

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import IMG from '../../assets/me.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm </h5>
+        <h1> Ryan Cho </h1>
+        <h5 className='text-light'> Junior Developer </h5>
+        <CTA />
+
+        <div className="me">
+          <img src={IMG} alt='me'/>
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderSocials() {
+  return (
+    <div className='header__socials'>
+        <a href='https://linkedin.com' target="_black"></a>
+        <a href='https://github.com' target="_black"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
