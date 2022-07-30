ec335c73f01c0268b19eab649c92502822436c7a
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-07-29 23:30:45 -0700

    complete nav

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUser} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+
 
 const About = () => {
     return (
-        <div>About</div>
+        <section id='about'>
+            <h5> Get To Know </h5>
+            <h2> About Me </h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__icon'/>
+                            <h5>Experience</h5>
+                            <small>BCIT College</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <FiUser className='about__icon'/>
+                            <h5>Sample</h5>
+                            <small>BCIT College Projects</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon'/>
+                            <h5>Projects</h5>
+                            <small>BCIT College Projects</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        sample sample sample sample sample sample sample sample sample
+                        sample sample sample sample sample sample sample sample sample
+                        sample sample sample sample sample sample sample sample sample
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
