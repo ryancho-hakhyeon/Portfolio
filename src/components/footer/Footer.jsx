e1b754d8d158d92f2622e0fc3044fab6cc6c9db4
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-07-31 01:52:04 -0700

    complete footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+//import {} from 'react-icons/'
+//import {} from 'react-icons/'
+//import {} from 'react-icons/'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__log'>RyanCho</a>
+      <ul className='permalinks'>
+        <li><a href="#"></a>Home</li>
+        <li><a href="#about"></a>About</li>
+        <li><a href="#experience"></a>Experience</li>
+        <li><a href="#services"></a>Services</li>
+        <li><a href="#portfolio"></a>Portfolio</li>
+        <li><a href="#contact"></a>Contact</li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; RyanCho Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
