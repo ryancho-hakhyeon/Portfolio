f6f90723a26779b33b3009890b6559f47c35fdb9
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2023-10-24 22:47:44 -0700

    edit header

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
 import { FaGithub } from 'react-icons/fa'
 
